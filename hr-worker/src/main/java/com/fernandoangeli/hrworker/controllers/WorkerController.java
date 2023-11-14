20f66ee856eab7b9d82107b228542ea4366fb2fd
fernando-angeli <fernando_angeli@hotmail.com>
2023-11-13 22:25:56 -0300

    Zuul timeout

--- hr-worker/src/main/java/com/fernandoangeli/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/fernandoangeli/hrworker/controllers/WorkerController.java
@@ -34,13 +34,13 @@ public class WorkerController {
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
 
-        /*
+
         try {
             Thread.sleep(3000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        */
+
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
