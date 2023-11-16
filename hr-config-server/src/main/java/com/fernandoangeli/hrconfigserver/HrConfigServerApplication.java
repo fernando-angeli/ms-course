d326c5deb24ab58a671850288b8842849df49cf8
fernando-angeli <fernando_angeli@hotmail.com>
2023-11-15 22:55:33 -0300

    Private Git repository

--- hr-config-server/src/main/java/com/fernandoangeli/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/fernandoangeli/hrconfigserver/HrConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.fernandoangeli.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
