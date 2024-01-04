85a76a9081dbd3ebfd489515a94cc303a942d982
fernando-angeli <fernando_angeli@hotmail.com>
2024-01-04 09:12:14 -0300

    final project corrections

--- hr-config-server/src/main/java/com/fernandoangeli/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/fernandoangeli/hrconfigserver/HrConfigServerApplication.java
@@ -1,7 +1,5 @@
 package com.fernandoangeli.hrconfigserver;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
